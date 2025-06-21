8347ded92836653424f495d5a9be4630b421a855
mirzawajihali <mirzawajihali00@gmail.com>
2025-06-21 20:53:43 +0600

    feat get borrowed details using aggregation

--- src/app/controllers/borrows.controller.ts
+++ src/app/controllers/borrows.controller.ts
@@ -10,7 +10,7 @@ borrowsRouter.post('/', async (req: Request, res: Response) => {
     try {
         const borrowData = req.body;
         
-        // Use the static method to handle both operations
+        // Used the static method to handle both operations
         const data = await Borrow.createBorrowAndUpdateBook(borrowData);
         
         res.status(201).json({
@@ -24,5 +24,58 @@ borrowsRouter.post('/', async (req: Request, res: Response) => {
             message: error.message || "Failed to borrow book",
             error
         });
+    }
+});
+
+
+borrowsRouter.get("/", async (req: Request, res: Response) => {
+    try {
+        const data = await Borrow.aggregate([
+            {
+                $group :{
+                    _id : "$book",
+                    totalQuantity: { $sum: "$quantity" },
+                }
+            },
+
+            {
+                $lookup : {
+                    from : "books",
+                    localField : "_id",
+                    foreignField : "_id",
+                    as : "book"
+                }
+            },
+            {
+                $unwind: "$book"
+            },
+
+            {
+                $project : {
+                     _id: 0,
+                    book: {
+                        title: "$book.title",
+                        isbn: "$book.isbn"
+                    },
+                    totalQuantity: 1
+                }
+            }
+
+        ]);
+        res.status(200).json({
+            success: true,
+            message: "Borrowed books summary retrieved successfully",
+            data
+        });
+
+        
+    }
+    catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to retrieve borrowed books summary",
+            error: error.message || error
+        });
+
     }
 });
